ca491319b94dcf0c5f3c2d2016a38be867e2f986
Higor_Carlos <higorcarlos252@gmail.com>
2024-03-20 12:14:18 -0300

    meu terceiro commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ola, mundo!");
+        System.out.println("Ola, mundo! 20/03/2024");
     }
 }
